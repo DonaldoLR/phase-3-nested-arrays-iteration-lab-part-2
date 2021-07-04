abb46f416119d9657966b4c12336797a556b3cd3
github username <github email address>
2021-07-03 23:47:45 -0400

    more practice with iterations of nested arrays

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,15 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  results_array = []
+  src.each do |day|
+    lowest_temp = day[0]
+    day.each do |hour|
+      if hour < lowest_temp 
+        lowest_temp = hour
+      end
+    end
+    results_array << lowest_temp
+  end
+  results_array
 end
